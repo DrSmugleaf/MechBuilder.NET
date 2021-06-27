0c797505215c39089691b0314d2082ced4d4e22c
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-06-27 23:08:48 +0200

    Create MechBuilder.NET.Web project

---
+++ MechBuilder.NET.Web/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MechBuilder.NET.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                // endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            });
+        }
+    }
+}
---
+++ MechBuilder.NET.Web/Views/Home/Index.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MechBuilder.NET.Web.Views.Home
+{
+    public class Index : PageModel
+    {
+    }
+}
